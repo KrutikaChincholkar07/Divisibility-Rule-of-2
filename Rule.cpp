a2c0e6374a4dee96c8a002a4462475ecb0693a27
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2026-02-03 23:05:37 +0530

    Implement divisibility check for user input number
    Refactored code to include input/output functionality for checking divisibility by 2.
    Logic Used:
    Take number from user
    Find last digit using: num % 10
    If last digit is even → divisible by 2
    Examples:
    246 → last digit 6 → divisible by 2 
    135 → last digit 5 → not divisible by 2 
    
    Input: Enter a number: 246
    Output: 246 is divisible by 2

---
+++ Rule.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int lastDigit = num % 10;
+
+    if (lastDigit % 2 == 0)
+        cout << num << " is divisible by 2";
+    else
+        cout << num << " is not divisible by 2";
+
+    return 0;
+}
